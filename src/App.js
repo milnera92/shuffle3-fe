61736b80f170ed1a6b37aaec9dff9399e47ffe42
Andrew Milner <95140821+milnera92@users.noreply.github.com>
2023-03-02 20:12:33 -0500

    added style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color:  #FFDFD3;
+`;
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 400px;
+  padding: 2rem;
+  background-color: #ffffcc;
+  border: 8px solid #000000;
+  box-shadow: 10px 10px 0px #000000;
+  margin: 2rem;
+  background-color: beige;
+`;
+
+const Title = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+`;
+
+const Content = styled.p`
+  font-size: 1.2rem;
+  margin-bottom: 2rem;
+`;
+
+const Audio = styled.audio`
+  margin-top: 1rem;
+`;
 
 function App() {
-  // const [rssData, setRssData] = useState(null);
   const [randomItem, setRandomItem] = useState(null);
 
   useEffect(() => {
@@ -9,26 +46,24 @@ function App() {
     fetch('https://shuffle3-be.onrender.com/rss')
       .then(response => response.json())
       .then(data => {
-        // setRssData(data);
         setRandomItem(data.items[Math.floor(Math.random() * data.items.length)]);
       });
   }, []);
 
   if (!randomItem) {
-    return <div>Loading...</div>;
+    return <Container>Loading...</Container>;
   }
 
   return (
-    <div>
+    <Container>
       {randomItem && (
-        <div>
-          <h2>{randomItem.title}</h2>
-          <p>{randomItem.contentSnippet}</p>
-
-          <audio src={randomItem.enclosure.url} controls></audio>
-        </div>
+        <Card>
+          <Title>{randomItem.title}</Title>
+          <Content>{randomItem.contentSnippet.substring(0, randomItem.contentSnippet.length - 91)}</Content>
+          <Audio src={randomItem.enclosure.url} controls></Audio>
+        </Card>
       )}
-    </div>
+    </Container>
   );
 }
 
