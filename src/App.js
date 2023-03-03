612880013f1b5bab27c3f2d229a07dbefd15418a
Andrew Milner <95140821+milnera92@users.noreply.github.com>
2023-03-02 20:45:44 -0500

    Adding loading stuff, change style

--- src/App.js
+++ src/App.js
@@ -1,52 +1,87 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
+  height: 100%;
+  background-color: #fef4e6;
+  padding: 16px;
 `;
 
 const Card = styled.div`
-  background-color: #ff8080;
-  border: 8px solid black;
-  border-radius: 10px;
-  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.5);
-  width: 80%;
-  padding: 1rem;
-  margin-bottom: 2rem;
-
-  @media (max-width: 768px) {
-    width: 90%;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #bde4c4;
+  border: 4px solid #000;
+  border-radius: 8px;
+  padding: 16px;
+  margin: 16px 0;
+  box-shadow: 4px 4px 0 #000;
+  width: 100%;
+  max-width: 500px;
 `;
 
 const Title = styled.h2`
   font-size: 2rem;
   margin-bottom: 1rem;
+  color: #000;
 `;
 
 const Content = styled.p`
   font-size: 1.2rem;
   margin-bottom: 2rem;
+  color: #000;
 `;
 
-const PlayButton = styled.audio`
-  display: block;
-  margin: auto;
+const Button = styled.button`
+  font-size: 1.2rem;
+  background-color: #f5b5d9;
+  border: 4px solid #000;
+  color: #000;
+  border-radius: 8px;
+  padding: 8px 16px;
+  cursor: pointer;
+  margin-top: 16px;
 `;
 
 const Loading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  color: #000;
   font-size: 2rem;
-  font-weight: bold;
-  text-align: center;
-  margin-bottom: 2rem;
+
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 4px solid #000;
+  border-top-color: #f5b5d9;
+  animation: ${rotate} 1s linear infinite;
 `;
 
 function App() {
   const [randomItem, setRandomItem] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetch('https://shuffle3-be.onrender.com/rss')
@@ -56,21 +91,37 @@ function App() {
       });
   }, []);
 
-  if (!randomItem) {
-    return (
-      <Container>
-        <Loading>Loading...</Loading>
-      </Container>
-    );
-  }
+  const handleRefresh = () => {
+    setLoading(true);
+    fetch('https://shuffle3-be.onrender.com/rss')
+      .then(response => response.json())
+      .then(data => {
+        setRandomItem(data.items[Math.floor(Math.random() * data.items.length)]);
+        setLoading(false);
+      });
+  };
 
   return (
     <Container>
-      <Card>
-        <Title>{randomItem.title}</Title>
-        <Content>{randomItem.contentSnippet.substring(0, randomItem.contentSnippet.length - 91)}</Content>
-        <PlayButton src={randomItem.enclosure.url} controls></PlayButton>
-      </Card>
+      {randomItem && (
+        <Card>
+          <Title>{randomItem.title}</Title>
+          <Content>{randomItem.contentSnippet.substring(0, randomItem.contentSnippet.length - 91)}</Content>
+
+          <audio src={randomItem.enclosure.url} controls></audio>
+
+          <Button onClick={handleRefresh}>
+            {loading ? <Spinner /> : "Refresh"}
+          </Button>
+        </Card>
+      )}
+
+      {!randomItem && (
+        <Card>
+          {loading && <Spinner />}
+          {!loading && <Loading>Loading...</Loading>}
+        </Card>
+      )}
     </Container>
   );
 }
