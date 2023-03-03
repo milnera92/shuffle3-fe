0e32e0bb5df72ccbc2dc1102619f5f19c93001d6
Andrew Milner <95140821+milnera92@users.noreply.github.com>
2023-03-02 20:23:06 -0500

    more style, mobile and loading

--- src/App.js
+++ src/App.js
@@ -7,21 +7,20 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
-  background-color:  #FFDFD3;
 `;
 
 const Card = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 400px;
-  padding: 2rem;
-  background-color: #ffffcc;
-  border: 8px solid #000000;
-  box-shadow: 10px 10px 0px #000000;
-  margin: 2rem;
-  background-color: beige;
+  background-color: #ff8080;
+  border: 8px solid black;
+  border-radius: 10px;
+  box-shadow: 5px 5px 15px rgba(0, 0, 0, 0.5);
+  width: 80%;
+  padding: 1rem;
+  margin-bottom: 2rem;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
 `;
 
 const Title = styled.h2`
@@ -34,15 +33,22 @@ const Content = styled.p`
   margin-bottom: 2rem;
 `;
 
-const Audio = styled.audio`
-  margin-top: 1rem;
+const PlayButton = styled.audio`
+  display: block;
+  margin: auto;
+`;
+
+const Loading = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 2rem;
 `;
 
 function App() {
   const [randomItem, setRandomItem] = useState(null);
 
   useEffect(() => {
-    console.log('useEffect called');
     fetch('https://shuffle3-be.onrender.com/rss')
       .then(response => response.json())
       .then(data => {
@@ -51,18 +57,20 @@ function App() {
   }, []);
 
   if (!randomItem) {
-    return <Container>Loading...</Container>;
+    return (
+      <Container>
+        <Loading>Loading...</Loading>
+      </Container>
+    );
   }
 
   return (
     <Container>
-      {randomItem && (
-        <Card>
-          <Title>{randomItem.title}</Title>
-          <Content>{randomItem.contentSnippet.substring(0, randomItem.contentSnippet.length - 91)}</Content>
-          <Audio src={randomItem.enclosure.url} controls></Audio>
-        </Card>
-      )}
+      <Card>
+        <Title>{randomItem.title}</Title>
+        <Content>{randomItem.contentSnippet.substring(0, randomItem.contentSnippet.length - 91)}</Content>
+        <PlayButton src={randomItem.enclosure.url} controls></PlayButton>
+      </Card>
     </Container>
   );
 }
